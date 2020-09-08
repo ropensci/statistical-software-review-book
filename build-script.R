1e3a3300dff4e190d50a4044ac4fa4e996d6ec3c
mpadge <mark.padgham@email.com>
2020-09-08 11:00:57 +0200

    also render pdf in build script [ci skip]

--- build-script.R
+++ build-script.R
@@ -2,5 +2,5 @@
 
 source ("scripts/getZoteroNotes.R")
 Sys.setenv("CHROMOTE_CHROME" = "/usr/bin/google-chrome-stable")
-#bookdown::render_book ("index.Rmd", output_format = c ("html_document", "pdf_document"))
-bookdown::render_book("index.Rmd")
+#bookdown::render_book("index.Rmd")
+bookdown::render_book("index.Rmd", output_format = c ("bookdown::gitbook", "bookdown::pdf_book"))
