70e5b3d7a12e99c8eacfaa728cc5975eed999ef1
mpadge <mark.padgham@email.com>
2020-09-29 14:04:35 +0200

    switch off pdf generation on local build-script

--- build-script.R
+++ build-script.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
-source ("scripts/getZoteroNotes.R")
+source("scripts/getZoteroNotes.R")
 Sys.setenv("CHROMOTE_CHROME" = "/usr/bin/google-chrome-stable")
-#bookdown::render_book("index.Rmd")
-bookdown::render_book("index.Rmd", output_format = c ("bookdown::gitbook", "bookdown::pdf_book"))
+# bookdown::render_book("index.Rmd")
+# bookdown::render_book("index.Rmd", output_format = c ("bookdown::gitbook", "bookdown::pdf_book"))
+bookdown::render_book("index.Rmd", output_format = c("bookdown::gitbook"))
