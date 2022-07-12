cdab5deb49162d757bb39e1826486921c4fc82bd
mpadge <mark.padgham@email.com>
2022-07-12 18:41:12 +0200

    start quarto adaptation for #64

--- build-script.R
+++ build-script.R
@@ -2,6 +2,15 @@
 
 source("scripts/getZoteroNotes.R")
 Sys.setenv("CHROMOTE_CHROME" = "/usr/bin/google-chrome-stable")
+
+# Put version from DESC file in quarto `_vars` file:
+d <- data.frame (read.dcf (file.path (here::here (),
+                                      "DESCRIPTION")))
+version <- d$Version
+if (length (gregexpr ("\\.", version) [[1]]) > 2) {
+    version <- gsub ("\\.\\d{3}$", "", version, perl = TRUE)
+}     
+writeLines (paste0 ("version: ", version), "_variables.yml")
 # bookdown::render_book("index.Rmd")
 # bookdown::render_book("index.Rmd", output_format = c ("bookdown::gitbook", "bookdown::pdf_book"))
-bookdown::render_book("index.Rmd", output_format = c("bookdown::bs4_book"))
+quarto::quarto_render ()
