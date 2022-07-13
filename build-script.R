73a7aedcbb73c1b7c27a17a5f3546e4faae68427
mpadge <mark.padgham@email.com>
2022-07-13 11:18:20 +0200

    cache quarto build for #64

--- build-script.R
+++ build-script.R
@@ -11,6 +11,5 @@ if (length (gregexpr ("\\.", version) [[1]]) > 2) {
     version <- gsub ("\\.\\d{3}$", "", version, perl = TRUE)
 }     
 writeLines (paste0 ("version: ", version), "_variables.yml")
-# bookdown::render_book("index.Rmd")
-# bookdown::render_book("index.Rmd", output_format = c ("bookdown::gitbook", "bookdown::pdf_book"))
-quarto::quarto_render ()
+
+quarto::quarto_render (cache = TRUE)
